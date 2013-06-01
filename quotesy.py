2f52e55adf6d973fca0dc8c81994fa09ee4e15ee
qnoox <qnooxapps@gmail.com>
2013-06-01 15:40:05 +0200

    rewrite

--- quotesy.py
+++ quotesy.py
@@ -1,31 +1,27 @@
-import urllib as p
-import re
-from random import randrange
+#!/usr/bin/python2
+#from random import randrange
+import random
 import subprocess
-
-sock = p.urlopen("http://www.brainyquote.com/" +
-                 "quotes/topics/topic_intelligence.html")
-grabSource = sock.read()
-
-sock.close()
-
-#uncomment this line if you want to grab without link
-#matches = re.findall('<span class="bqQuoteLink">
-#<a title"view quote">(.*?)</a></span><br>')
-##EndOfCommentedCode
-
-matches = re.findall('<span class="bqQuoteLink"><a title=(.*?)</a></span><br>',
-                     grabSource, re.S)
-
-#holder = list()
-#holder.append(matches)
+import urllib
+import re
 
 
-#remove crap from match and print result!
-n = randrange(20)
-removeextra = matches[n].split()[3::]
-result = ' '.join(removeextra)
+def sockethandler():
+    sock = urllib.urlopen("http://www.brainyquote.com/" +
+                          "quotes/topics/topic_intelligence.html")
+    grabSource = sock.read()
+    sock.close()
 
-#We now call open the call from subprocess and use cowsay with tux
-#and put in the quote tada!
-subprocess.call(['cowsay', '-f', '/usr/share/cows/tux.cow', result])
+    matches = re.findall(
+        '<span class="bqQuoteLink"><a title=(.*?)</a></span><br>',
+        grabSource, re.S)
+    #n = randrange(20)
+    n = random.choice(
+        [1, 2, 3, 5, 9, 12, 20, 14, 25, 22, 4, 6, 13, 23, 24, 19, 21])
+    removeextra = matches[n].split()[3::]
+    result = ' '.join(removeextra)
+    #return result
+    subprocess.call(['cowsay', '-f', '/usr/share/cows/tux.cow', result])
+    return
+
+sockethandler()
